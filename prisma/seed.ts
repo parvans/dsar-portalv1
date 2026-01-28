607e3ec9031e54dbbe00c348828ef29f06ab6a8d
parvans <parvansajeevan@gmail.com>
2026-01-28 22:13:57 +0530

    login page

---
+++ prisma/seed.ts
@@ -0,0 +1,50 @@
+import {PrismaClient} from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
+
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+    adapter,
+});
+
+const mainfunc = async()=>{
+    const adminPassword = await bcrypt.hash('admin@123',10);
+    const ownerPassword = await bcrypt.hash('owner@123',10);
+
+    await prisma.user.upsert({
+        where:{email:'admin@dsar.com'},
+        update:{},
+        create:{
+            email:'admin@dsar.com',
+            password:adminPassword,
+            role:'admin'
+        }
+    });
+
+    await prisma.user.upsert({
+        where:{email:'owner@dsar.com'},
+        update:{},
+        create:{
+            email:'owner@dsar.com',
+            password:ownerPassword,
+            role:'owner'
+        }
+    });
+
+    console.log("Seed admin and owner");
+
+}
+
+mainfunc()
+.catch((error)=>{
+    console.log("Error",error);
+    
+})
+.finally(async()=>{
+    await prisma.$disconnect()
+})
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { useState } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from '@/components/ui/input';
+import { Alert } from '@/components/ui/alert';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+
+    const handleSubmit = async()=>{
+        setError('');
+        try {
+            const res = await fetch('/ap/auth/login',{
+                method:"POST",
+                body:JSON.stringify({email, password}),
+            });
+
+            if(!res.ok){
+                setError("Invalid Credentials");
+                return;
+            }
+
+            const {role} = await res.json();
+            router.push(role === 'admin' ? "/admin" : '/owner')
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Card className='w-full max-w-sm'>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+            <div className='space-y-4'>
+                <Input
+                placeholder='Email'
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
+                />
+                <Input
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
+                />
+                {error && <Alert variant={"destructive"}>{error}</Alert>}
+                <Button 
+                 type='button' 
+                 className='w-full'
+                 onClick={handleSubmit}
+                 >Login</Button>
+            </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
